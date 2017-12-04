cf952e4322be4494a53d953266b68efec7b4c6dc
Brendan Meath <bren.meath@gmail.com>
2017-12-04 01:04:41 +0000

    Add comments

--- container-server.py
+++ container-server.py
@@ -42,11 +42,15 @@ def containers_index():
 	curl -s -X GET -H 'Accept: application/json' http://localhost:8080/containers?state=running
 
 	"""
+	# check if state parameter was supplied with value running
 	if request.args.get('state') == 'running': 
+		# show only running containers
+		# use a go format template to get the output in TSV (so we can split the string on tab instead of space)
+		# if the output string was split on ' ' character, it would not work since details such as status contain spaces
 		output = docker('ps --format \'{{.ID}}\t{{.Image}}\t{{.Command}}\t{{.CreatedAt}}\t{{.Status}}\t{{.Ports}}\t{{.Names}}\'')
 		resp = jdumps(docker_ps_to_array(output))
 	else:
-		
+		# show all containers, not just running
 		cmd = ['docker', 'ps', '-a', '--format', '\'{{.ID}}\t{{.Image}}\t{{.Command}}\t{{.CreatedAt}}\t{{.Status}}\t{{.Ports}}\t{{.Names}}\'']
 		process = Popen(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True)
 		stdout, stderr = process.communicate()
@@ -67,6 +71,8 @@ def images_index():
 	"""
 	
 	output = docker('images')
+	
+	# get image list/details as a list and convert the list to json
 	resp = jdumps(docker_images_to_array(output))
 	
 	return Response(response=resp, mimetype="application/json")
@@ -79,9 +85,11 @@ def services_index():
 	curl -s -X GET -H 'Accept: application/json' http://localhost:8080/services
 	
 	"""
-	
+	# get ids of services
 	output = docker('service ls -q')
+	# create list out of newline separated values
 	services = output.split('\n')
+	# convert list to json document
 	resp = jdumps(services)
 	
 	return Response(response=resp, mimetype="application/json")
@@ -95,8 +103,11 @@ def nodes_index():
 	
 	"""
 	
+	# get ids of nodes
 	output = docker('node ls -q')
+	# create list out of newline separated values
 	nodes = output.split('\n')
+	# convert list to json document
 	resp = jdumps(nodes)
 	
 	return Response(response=resp, mimetype="application/json")
@@ -109,7 +120,10 @@ def containers_show(id):
 	curl -s -X GET -H 'Accept: application/json' http://localhost:8080/containers/<id>
 	
 	"""
+	
 	output = docker('inspect {}'.format(id))
+	
+	# no need to convert to json; this command already returns valid json
 	resp = output
 	return Response(response=resp, mimetype="application/json")
 
@@ -133,7 +147,10 @@ def images_remove(id):
 	curl -X DELETE http://localhost:8080/images/<id>
 	
 	"""
+	# remove image with given id
 	docker ('rmi {}'.format(id))
+	
+	# return id of deleted image
 	resp = '{"id": "%s"}' % id
 	return Response(response=resp, mimetype="application/json")
 
@@ -145,7 +162,10 @@ def containers_remove(id):
 	curl -X DELETE http://localhost:8080/containers/<id>
 	
 	"""
+	# remove container with given id
 	output = docker('rm {}'.format(id))
+	
+	# return id of deleted container
 	resp = '{"output": "%s"}' % output
 	return Response(response=resp, mimetype="application/json")
 
@@ -157,11 +177,14 @@ def containers_remove_all():
 	curl -X DELETE http://localhost:8080/containers
 
 	"""
+	# firstly get all containers
 	containers = docker('ps -aq').replace('\n', ' ')
 	
 	if containers != '':
+		# remove all containers in single command
 		output = docker('rm {}'.format(containers))
 		resp = '{"Deleted": ['
+		# build up a json document to store the ids of deleted containers
 		for id in output.split('\n'):
 			if id != '':
 				resp += '{{"id": "{}"}}, '.format(id)
@@ -181,12 +204,15 @@ def images_remove_all():
 
 	"""
 	
+	# firstly get all images
 	images = docker('images -aq').replace('\n', ' ')
 	if images != '':
+		# remove all images in single command
 		output = docker('rmi {}'.format(images))
 		resp = '{"Deleted": ['
 		for id in output.split('\n'):
 			if id != '':
+				# check if starts with deleted, since we must omit lines starting with "Untagged"
 				if id.startswith('Deleted'):
 					resp += '{{"id": "{}"}}, '.format(id.split(':')[2])
 		resp = resp[:-2] # remove extra comma
@@ -208,15 +234,21 @@ def containers_run():
 
 	"""
 	
+	# beginning of docker arguments to run container
 	args = 'run -d '
 	
+	# get json document contained in POST request
 	body = request.get_json(force=True)
+	
+	# check if image was specified
 	if 'image' in body:
 		args += '{} '.format(body['image'])
+		# check if any ports will be published/exposed
 		if 'publish' in body:
 			args += '-p {}'.format(body['publish'])
-		#id = dockerMultipleParams(*(args + (image,)))[0:12]
 		id = docker(args)
+		
+		# put id of newly created container in response
 		resp = '{{"id": "{}"}}'.format(id.replace('\n',''))
 	else:
 		resp = '{"Error":"no value specified for "image" attribute"}'
@@ -231,24 +263,35 @@ def images_create():
 	curl -H 'Accept: application/json' -F file=@Dockerfile http://localhost:8080/images
 
 	"""
+
 	dockerfile_path = path.join(app.config['UPLOAD_DIR'], 'Dockerfile')
+	
+	# check if 'file' value was provided in POST request
 	if 'file' not in request.files:
 		output = '{"error": "No file uploaded"}'
 	else:
+		# get the file object
 		dockerfile = request.files['file']
 		if dockerfile.filename == '':
 			output = '{"error": "No selected file"}'
 		else:
+			# save the file so docker can read it later
 			dockerfile.save(dockerfile_path)
+			
+			# run docker build command using uploaded file as Dockerfile
 			output = docker('build {}'.format(app.config['UPLOAD_DIR']))
 	
+	# Dockerfile no longer needed. Delete it
 	remove(dockerfile_path)
 	
+	# check output for id of newly built image
 	match = re.search('Successfully built (.+?)\n', output)
 	if match:
+		# Build was successful
 		id = match.group(1)
 		output = '{{"id": "{}"}}'.format(id)
 	else:
+		# Build was unsuccessful
 		output = '{"Error": "Build unsuccessful"}'
 	
 	resp = output
@@ -266,13 +309,18 @@ def containers_update(id):
 	
 	output = ''
 	
+	# get json document specifying container attributes to modify
 	body = request.get_json(force=True)
 	state = body['state']
 	
 	if state == 'running':
+		# caller wants to run container. Restart it.
 		output = docker('restart {}'.format(id))
 	if state == 'stopped':
+		# caller wants to stop container. Stop it.
 		output = docker('stop {}'.format(id))
+	
+	# put id of modified container in response
 	resp = '{{"id": "{}"}}'.format(output)
 	
 	return Response(response=resp, mimetype="application/json")
@@ -288,18 +336,27 @@ def images_update(id):
 	
 	output = ''
 	
+	# get json document specifying attributes of the image to modify
 	body = request.get_json(force=True)
 	tag = body['tag'].lower()
 	
+	# create a new tag for the image
 	output = docker('image tag {} {}'.format(id, tag))
 	
 	if output == '':
+		# no text in response indicates success
 		resp = '{"status": "success"}'
 	else:
+		# something went wrong, so put the program output in the json response data
 		resp = '{{"status": "failure", "output": "{}"}}'.format(output)
 	
 	return Response(response=resp, mimetype="application/json")
 
+# run the docker program with given arguments
+# Parameters:
+#		args: a string of whitespace-separated arguments to the program
+# Returns:
+#		All data from STDERR and STDOUT
 def docker(args):
 	cmd = ['docker']
 	cmd += args.split()
@@ -314,7 +371,10 @@ def docker(args):
 
 #
 # Parses the output of a Docker PS command to a python List
-# 
+# Parameters:
+#		output: the output after runnning 'docker ps --format '{{.ID}}\t{{.Image}}\t{{.Command}}\t{{.CreatedAt}}\t{{.Status}}\t{{.Ports}}\t{{.Names}}''
+# Returns:
+#		a Python list of Container details
 def docker_ps_to_array(output):
 	all = []
 	for c in [line.split('\t') for line in output.splitlines()[1:]]:
@@ -333,7 +393,11 @@ def docker_ps_to_array(output):
 
 #
 # Parses the output of a Docker logs command to a python Dictionary
-# (Key Value Pair object)
+# Parameters:
+#		id: string for id or string for sha256 hash of a container
+# 		output: string result of running 'docker logs <id>'
+# Returns:
+#		string of output of docker logs as a json document
 def docker_logs_to_object(id, output):
 	logs = {}
 	logs['id'] = id
@@ -345,7 +409,10 @@ def docker_logs_to_object(id, output):
 
 #
 # Parses the output of a Docker image command to a python List
-# 
+# Parameters:
+#		output: a string of output from running 'docker images'
+# Returns:
+#		a Python list of Image details
 def docker_images_to_array(output):
 	all = []
 	for c in [line.split() for line in output.splitlines()[1:]]:
@@ -360,5 +427,5 @@ if __name__ == "__main__":
 	# create folder to store POST request files
 	if not path.exists(app.config['UPLOAD_DIR']):
 		makedirs(app.config['UPLOAD_DIR'])
-	
+	# begin listening for connection requests
 	app.run(host="0.0.0.0",port=8080, debug=True)
